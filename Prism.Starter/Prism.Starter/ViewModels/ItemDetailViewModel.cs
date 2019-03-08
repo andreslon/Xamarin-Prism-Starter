1820c011531c07402674185104d003cafeceb414
Andrés Londoño <andres.londono@teaminternational.com>
2019-03-08 16:54:56 -0500

    Initial project Xamarin Forms with master detail template

---
+++ Prism.Starter/Prism.Starter/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Prism.Starter.Models;
+
+namespace Prism.Starter.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
